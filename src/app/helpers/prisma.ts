2bdd66f4428ff1c107c3d6a3de34a23cf0c5cd20
Mobinul Islam Mahi <mobinulislammahi@gmail.com>
2026-05-25 02:40:58 +0600

    feat(vercel): add config for maxDuration and remove unnecessary headers

--- src/app/helpers/prisma.ts
+++ src/app/helpers/prisma.ts
@@ -1,9 +1,11 @@
-import dotenv from "dotenv";
-import path from "path";
-import { PrismaClient } from "../../generated/prisma";
+import { PrismaClient } from "@prisma/client";
 
-dotenv.config({ path: path.resolve(__dirname, "../../../.env") });
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-const prisma = new PrismaClient();
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
